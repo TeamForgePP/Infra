d24ee07a989a6ba3ef705c5d54b158bb89c78ba4
yurtsev <yurtsevburiy@gmail.com>
2026-01-12 14:56:19 +0900

    chore(minio): упростил init-скрипт и оставил настройку публичного бакета

--- minio/init/init.sh
+++ minio/init/init.sh
@@ -6,8 +6,6 @@ mc alias set storage http://minio:9000 "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWO
 mc mb -p "storage/${MINIO_BUCKET_PUBLIC}" || true
 mc mb -p "storage/${MINIO_BUCKET_PRIVATE}" || true
 
-# Если тебе нужны “публичные ссылки без авторизации” — оставь эту строку.
-# Если НЕ нужно — закомментируй, и всё будет только через presigned.
 mc anonymous set download "storage/${MINIO_BUCKET_PUBLIC}" || true
 
 echo "MinIO init done."
